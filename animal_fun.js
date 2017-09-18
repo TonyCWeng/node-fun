2271e083c5c76c81367cde06766ec5c0f5adeb73
tonycweng <tcweng91@gmail.com>
2017-09-18 16:58:41 -0700

    ensure capitalization

--- animal_fun.js
+++ animal_fun.js
@@ -5,10 +5,22 @@ const fs = require('fs');
 //     console.log(err);
 //   } else {
 //     console.log(data);
+//     var animals = data;
 //   }
 // });
 
-// fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     // console.log(data);
+//     var animals = data;
+//     animals.split('\n').filter(animal => animal.startsWith("a")).join('\n');
+//     console.log(animals);
+//   }
+// });
+
+// fs.writeFile('./example.txt', 'a', err => {
 //   if (err) {
 //     console.log(err);
 //   } else {
@@ -16,7 +28,31 @@ const fs = require('fs');
 //   }
 // });
 
-console.log(process.argv);
+// console.log(process.argv[4]);
 
 // process.argv should consist of array of two arguments: the absolute
 // paths of the Node executeable and the file.
+
+function filterAnimals(animals, letter) {
+  return animals
+    .split('\n')
+    .filter(animal => animal.startsWith(letter))
+    .join('\n');
+}
+var letter = process.argv[2].toUpperCase();
+
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const animals = filterAnimals(data, letter);
+
+  fs.writeFile(`${letter}_animals.txt`, animals, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`successfully created ${letter}_animals.txt`);
+  });
+});
